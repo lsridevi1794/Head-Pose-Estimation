ecc3654f3e11297b4364fa34a9388e4b30a860d1
lsridevi1794 <48228252+lsridevi1794@users.noreply.github.com>
2019-03-07 00:08:10 -0600

    Training for pan angles

---
+++ main_Pan.py
@@ -0,0 +1,73 @@
+import input_fn, model, training
+import numpy as np
+import math
+import os
+import tensorflow as tf
+import model
+import evaluate
+
+# Just disables the warning, doesn't enable AVX/FMA
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# run this on gpu
+tf.device("/gpu:1")
+
+ROOT_PATH = "../HeadPoseImageDatabase"
+train_data_directory = os.path.join(ROOT_PATH, "training")
+batch_size = 256
+
+label_csv_path = train_data_directory+"/prima_label.csv"
+filenames = list(np.genfromtxt(label_csv_path, delimiter=',', skip_header=1, usecols=(0), dtype=None))
+labels= list(np.genfromtxt(label_csv_path, delimiter=',', skip_header=1, usecols=6, dtype=None))
+# X = tf.placeholder(tf.float32, shape=[None,64,64,3])
+# Y = tf.placeholder(tf.float32, shape=[None,9])
+
+# X = inputs['images']
+# Y = inputs['labels']
+
+X = tf.placeholder(name= 'ip', dtype=tf.float32, shape=(None, 64,64,1))
+Y = tf.placeholder(tf.int32, [None,1])
+# network = model.model(X_train)
+network = model.cnn_model(X)
+[optimizer,cost] = training.trainer(network, Y)
+# print(optimizer) 
+
+# Initialization
+sess = tf.Session()
+num_points = len(filenames)
+sess.run(tf.global_variables_initializer())
+# Run training
+for epoch in range(10000):
+    for jj in range(int(math.floor((num_points // batch_size) - 1))):
+        # Get the data
+
+        inputs = input_fn.input_fn(filenames, labels, batch_size)
+        sess.run(inputs['iterator_init_op'])
+        train_X = sess.run(inputs['images'])
+        train_Y = sess.run(inputs['labels'])
+        train_Y = np.array(train_Y)
+        train_Y = train_Y.reshape((batch_size,1))
+
+        # input_to_sess = {X:train_X, Y:train_Y}
+        temp = sess.run(X, feed_dict={X:train_X})
+        sess.run(optimizer,feed_dict = {X:train_X, Y:train_Y})
+        # print('Done with batch {} and epoch {}'.format(jj,epoch))
+
+    # Evaluate the model
+    if (epoch % 10) == 0:
+        train_X = sess.run(inputs['images'])
+        train_Y = sess.run(inputs['labels'])
+        train_Y = np.array(train_Y)
+        train_Y = train_Y.reshape((batch_size,1))
+
+        output = sess.run(network, feed_dict = {X:train_X})
+        pred = tf.argmax(output,1)
+        corr_pred = tf.equal(pred, train_Y)
+        accuracy_operation = tf.reduce_mean(tf.cast(corr_pred,
+        tf.float32))
+        for class_label in range(3):
+            class_corr_pred = tf.equal(pred==class_label,train_Y==class_label)
+            class_accuracy = tf.reduce_mean(tf.cast(class_corr_pred,tf.float32))
+            print("Accuracy after epoch {} for class_label {} is {}".format(epoch, class_label, sess.run(class_accuracy)))
+        print('Accuracy after epoch {} is {}'.format(epoch,
+        sess.run(accuracy_operation))) 
